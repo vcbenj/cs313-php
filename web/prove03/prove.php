d7c572bb2e3fe0db77a568ea1c3a2d6acbbce271
vcbenj <whi15026@byui.edu>
2020-01-25 16:55:17 -0700

    Php her we go

--- web/prove03/prove.php
+++ web/prove03/prove.php
@@ -38,7 +38,7 @@ body {
   <th> Description  
   </tr>
   <tr>
-  <td>  <input type="button" name="item_0" value="Add to Cart" id ="item_0" onclick="<?phpaddList();?>"> 
+  <td>  <input type="button" name="item_0" value="Add to Cart" id ="item_0" onclick="<?php addList(); ?>"> 
   <th> Laptop Dell 
   <th> $499
   <th> Dell XPS
