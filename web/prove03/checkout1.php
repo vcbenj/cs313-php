0c9fd3aed310ac479fc1768acfa7925f444a1564
vcbenj <whi15026@byui.edu>
2020-01-25 23:57:45 -0700

    Php her we go

--- web/prove03/checkout1.php
+++ web/prove03/checkout1.php
@@ -14,11 +14,10 @@ session_start();
 	echo $place . "<br>";
 }
 echo "NEW STUFF <br>";
+$newList = [];
 foreach($_SESSION['shp'] as $value) {
-    echo $value . " <_- <br>";
     foreach($places as $place) {
-        echo $place . " <_- place <br>";
-        if ($value != $place) {
+        if ($value == $place) {
             echo "Hello <br>";
             $ind = array_search($value);
             array_splice($shp, 1, $ind);
