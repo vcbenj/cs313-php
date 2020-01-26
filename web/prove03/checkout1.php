ba2282ba6182145d6760b6e4e51fcc6086aa9288
vcbenj <whi15026@byui.edu>
2020-01-25 23:39:48 -0700

    Php her we go

--- web/prove03/checkout1.php
+++ web/prove03/checkout1.php
@@ -4,15 +4,10 @@ session_start();
    
     echo "<br>";
     echo $_SESSION['index'];
-    echo $_SESSION['shp'][3];
+    $shp = $_SESSION['shp'][3];
     $List = $_GET["0"];
     $places = $_GET["places"];
     
-    foreach($list as $value){
-        echo $value;
-        $index++;
-        $_SESSION['index']=$index;
-    }
     foreach ($places as $place)
 {
 	//$place_clean = htmlspecialchars($place);
