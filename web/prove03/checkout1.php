b9b014f079e0f4f3b3696a63dd8ae2cd6a1c5010
vcbenj <whi15026@byui.edu>
2020-01-25 22:56:12 -0700

    Php her we go

--- web/prove03/checkout1.php
+++ web/prove03/checkout1.php
@@ -2,10 +2,6 @@
 // Start the session
 session_start();
 
-
-    $List = $_GET["myList"];
-    $item_2 = $_GET["item_2"];
-    $list = explode(",", $List);
     echo "<br>";
     echo $_SESSION['index'];
     
