18518dd25fcb91db0d4cbba602452588679c4c79
vcbenj <whi15026@byui.edu>
2020-02-12 15:56:35 -0700

    hi

--- web/teach04/addScripture.php
+++ web/teach04/addScripture.php
@@ -32,19 +32,23 @@ $content = _POST['content'];
 $statement = $db->prepare("INSERT INTO public.scripture (book) VALUES(". $book . ")");
 $statement->execute();
 
-// // Go through each result
-//while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-// {
-// 	// The variable "row" now holds the complete record for that
-// 	// row, and we can access the different values based on their
-// 	// name
-// 	$book = $row['book'];
-// 	$chapter = $row['chapter'];
-// 	$verse = $row['verse'];
-// 	$content = $row['content'];
 
-// 	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
-// }
+$statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
+$statement->execute();
+
+// // Go through each result
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	// The variable "row" now holds the complete record for that
+	// row, and we can access the different values based on their
+	// name
+	$book = $row['book'];
+	$chapter = $row['chapter'];
+	$verse = $row['verse'];
+	$content = $row['content'];
+
+	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+}
 
 ?>
 
